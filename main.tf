dec7e52f770c3f2ec856ffeff0465fdd1a5d4978
Phani <Phani@gmail.com>
2024-03-12 11:25:21 +0530

    kkj1

--- main.tf
+++ main.tf
@@ -7,33 +7,45 @@ resource "rafay_project" "rafay_proj_new" {
   spec {
     default = false    
     cluster_resource_quota {
-      cpu_requests = "4000m"
-      memory_requests = "4096Mi"
-      cpu_limits = "8000m"
-      memory_limits = "8192Mi"
-      config_maps = "10"
-      persistent_volume_claims = "5"
-      services = "20"    
-      pods = "200"
-      replication_controllers = "10"
-      services_load_balancers = "10"
-      services_node_ports = "10"
-      storage_requests = "100Gi"
+      cpu_requests = var.cpu_requests
+      memory_requests = var.memory_requests
+      cpu_limits = var.cpu_limits
+      memory_limits = var.memory_limits              
+      pods = var.pods
+      services = var.services    
+      services_load_balancers = var.services_load_balancers
+      services_node_ports = var.services_node_ports
+      persistent_volume_claims = var.persistent_volume_claims
+      storage_requests = var.storage_requests
     }
-    default_cluster_namespace_quota {
-      cpu_requests = "1000m"
-      memory_requests = "1024Mi"
-      cpu_limits = "2000m"
-      memory_limits = "2048Mi"
-      config_maps = "5"
-      persistent_volume_claims = "2"
-      services = "10"
-      pods = "20"
-      replication_controllers = "4"
-      services_load_balancers = "4"
-      services_node_ports = "4"
-      storage_requests = "10Gi"
-    } 
+    # cluster_resource_quota {
+    #   cpu_requests = "4000m"
+    #   memory_requests = "4096Mi"
+    #   cpu_limits = "8000m"
+    #   memory_limits = "8192Mi"
+    #   config_maps = "10"
+    #   persistent_volume_claims = "5"
+    #   services = "20"    
+    #   pods = "200"
+    #   replication_controllers = "10"
+    #   services_load_balancers = "10"
+    #   services_node_ports = "10"
+    #   storage_requests = "100Gi"
+    # }
+    # default_cluster_namespace_quota {
+    #   cpu_requests = "1000m"
+    #   memory_requests = "1024Mi"
+    #   cpu_limits = "2000m"
+    #   memory_limits = "2048Mi"
+    #   config_maps = "5"
+    #   persistent_volume_claims = "2"
+    #   services = "10"
+    #   pods = "20"
+    #   replication_controllers = "4"
+    #   services_load_balancers = "4"
+    #   services_node_ports = "4"
+    #   storage_requests = "10Gi"
+    # } 
   }
 }
 
--- variables.tf
+++ variables.tf
@@ -37,3 +37,43 @@ variable "git_repo_name" {
 variable "git_repo_branch" {
   type = string  
 }
+
+variable "cpu_requests" {
+  type = string  
+}
+
+variable "memory_requests" {
+  type = string  
+}
+
+variable "cpu_limits" {
+  type = string  
+}
+
+variable "memory_limits" {
+  type = string  
+}
+
+variable "pods" {
+  type = string  
+}
+
+variable "services" {
+  type = string  
+}
+
+variable "services_load_balancers" {
+  type = string  
+}
+
+variable "services_node_ports" {
+  type = string  
+}
+
+variable "persistent_volume_claims" {
+  type = string  
+}
+
+variable "storage_requests" {
+  type = string  
+}
